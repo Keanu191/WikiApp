9008f4d60c504c30daaeedb536b8e8d9b35658b5
KEANU FARRO <30074191@tafe.wa.edu.au>
2024-02-20 08:18:20 +0800

    implemented add button 9.2

--- WikiApp/Form1.cs
+++ WikiApp/Form1.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿/*
+ * 30074191 / Keanu Farro
+ * Assesment 1 c# WikiAPP prototype
+ */
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +19,8 @@ namespace WikiApp
         // Define static variables for the dimensions 9.1
         public static int rows = 12;
         public static int columns = 4;
+        public int currentRow = 0; // Tracks the current row 9.2
+
 
         // Global 2D string array 9.1
         public static string[,] wikiArray = new string[rows, columns];
@@ -31,6 +37,26 @@ namespace WikiApp
         private void btnAdd_Click(object sender, EventArgs e)
         {
             // 9.2 Add button
+            if (currentRow < rows)
+            {
+                string textBoxes = textBox1.Text + textBox2.Text + textBox3.Text + textBox4.Text; // Combine all textboxes together into a string
+
+                // Store information from the 4 textboxes into the 2d array
+                for (int i = 0; i < columns; i++)
+                {
+                    wikiArray[currentRow, i] = wikiArray[currentRow, i] = textBoxes;
+                }
+
+                // Move to next row
+                currentRow++;
+                MessageBox.Show("Row " + currentRow + " added successfully!");
+
+            }
+            else
+            {
+                MessageBox.Show("2D Array is full!");
+            }
+
         }
 
         private void listView_SelectedIndexChanged(object sender, EventArgs e)
